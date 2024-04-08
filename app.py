35e1f95de2c6b45aa4ac06fb1b2a063d45646a48
Jithu2590 <166409574+Jithu2590@users.noreply.github.com>
2024-04-08 21:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,57 +2,46 @@ import openai
 import os
 from dotenv import load_dotenv
 import streamlit as st
-import PyPDF2
-from PyPDF2 import PdfReader
-import docx
-
-import json
-import requests
 
 load_dotenv(override=True)
-openai.api_key=os.getenv("OPEN_API_KEY")
-def read_pdf(file_path):
-    pdf = PdfReader(file_path)
-    text = ''
-    for page in pdf.pages:
-        text += page.extract_text()
-    return text
+api_key = os.getenv('OPEN_API_KEY')
 
-def read_docx(file_path):
-    doc = docx.Document(file_path)
-    text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
-    return text
+openai.api_key = api_key
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.chat.completions.create(
-        model=model,
-        messages=messages,
-        temperature=0
-    )
-    response_message = response.choices[0].message.content
-    return response_message
-
-# Streamlit UI
-st.title("Document-based Question Answering System")
-file_path = st.file_uploader("Upload PDF/Docx/txt",type=["pdf","docx","txt"])
-
-question = st.text_input('Enter your question:')
-
-
-if st.button('Get Answer'):
-    if file_path.type=='application/pdf':
-            document_text = read_pdf(file_path)
-    elif file_path.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            document_text = read_docx(file_path)
-    elif file_path.type=="text/plain":
-           document_text = file_path.read().decode('utf-8')
-    else:
-            st.error('Unsupported file format')
-            st.stop()
-
-    prompt = f"""Your task is to answer the question asked based on the document provided by the user and also subtopics if the user asks.\n\nDocument:```{document_text,question}```"""
-    answer = get_completion(prompt)
-    st.success(f"Answer: {answer}")
+def get_completion(prompt, text,model="gpt-3.5-turbo"):
+        messages = [{"role": "user", "content": prompt}]
+        with st.chat_message("user"):
+           st.markdown(text)
         
-  
+       
+        with st.chat_message("assistant"):
+          message_placeholder = st.empty()
+        with st.spinner("Generating....."):
+
+                response = openai.chat.completions.create(
+                model=model,
+                messages=messages,
+                temperature=0.6
+                )
+        response_message = response.choices[0].message.content
+        return response_message
+        
+              
+
+    
+st.title("Code Documentation")
+input_text = st.chat_input("Enter the code")
+
+
+if input_text:
+        prompt = f"""Your task is to generate overall techincal documentation and overall software documentation of the code and aslo check whether given code if it is not programming language then display it is a not code
+        Review:```{input_text}```
+        """
+        response = get_completion(prompt,input_text)
+        st.markdown(response)
+
+        
+
+
+
+
